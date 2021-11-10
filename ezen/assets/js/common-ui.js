63c6f6320d8d3eed8ff74cf82b3e1b0949001840
HyoJinKim93 <maria11097@naver.com>
2021-11-10 16:00:27 +0900

    Update common-ui.js

--- ezen/assets/js/common-ui.js
+++ ezen/assets/js/common-ui.js
@@ -18,10 +18,37 @@ $(function(){
             btnAll.classList.toggle("on")  
             allMenu.classList.toggle("on")  
         });  
-        setTimeout(function(){
+        
+        var mapContainer2 = document.getElementById('map2'), // 지도를 표시할 div 
+            mapOption2 = {
+                center: new kakao.maps.LatLng(37.48685337098062, 126.89734333067844), // 지도의 중심좌표
+                level: 3 // 지도의 확대 레벨
+            };
+
+        var map2 = new kakao.maps.Map(mapContainer2, mapOption2); // 지도를 생성합니다
+
+        // 마커가 표시될 위치입니다 
+        var markerPosition2 = new kakao.maps.LatLng(37.48685337098062, 126.89734333067844);
+
+        // 마커를 생성합니다
+        var marker2 = new kakao.maps.Marker({
+            position: markerPosition2
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker2.setMap(map2);
+
+
+        mapContainer2.style.width = '583px';
+        mapContainer2.style.height = '206px';
+
+        map2.relayout();
+
+        setTimeout(function () {
             map2.relayout();
-            map2.setCenter(new kakao.maps.LatLng(37.48685337098062, 126.89734333067844));
-          }, 0);  
+            // map.setCenter(new kakao.maps.LatLng(lat, lng));
+            // map.setLevel(2); 필요하면 레벨조정
+        }, 100);
     }
     allMenuEvent();
 })

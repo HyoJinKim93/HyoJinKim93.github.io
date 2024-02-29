9984681b55ee0311077384510229e06c9cd6d14b
HyoJinKim93 <maria11097@naver.com>
2024-02-29 20:26:28 +0900

    Add files via upload

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     function hoverImg(){
-        $('.seoulmilk, .ngo, .shinhan').on('mouseenter', function () {
+        $('.seoulmilk, .ngo, .shinhan, .slp').on('mouseenter', function () {
             $(this).addClass('on');
         })
-        $('.seoulmilk, .ngo, .shinhan').on('mouseout', function () {
+        $('.seoulmilk, .ngo, .shinhan, .slp').on('mouseout', function () {
             $(this).removeClass('on');
         })
     }

a5634efb2744a079baabc722838a52ad6f16f040
HyoJinKim93 <maria11097@naver.com>
2024-03-03 12:01:20 +0900

    Update main.js

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     function hoverImg(){
-        $('.seoulmilk, .ngo, .shinhan, .slp').on('mouseenter', function () {
+        $('.seoulmilk, .ngo, .shinhan, .slp, .pdf').on('mouseenter', function () {
             $(this).addClass('on');
         })
-        $('.seoulmilk, .ngo, .shinhan, .slp').on('mouseout', function () {
+        $('.seoulmilk, .ngo, .shinhan, .slp, pdf').on('mouseout', function () {
             $(this).removeClass('on');
         })
     }
